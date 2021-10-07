ec30ebd6d0aa830faff35c621eeefb6950f81e4a
spadeninja <quanshuli.dev@gmail.com>
2021-10-07 19:43:10 +1000

    Update cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -12,6 +12,7 @@ class Cart(object):
             # create an empty cart
             cart = self.session[settings.CART_SESSION_ID] = {} 
         self.cart = cart 
+
     def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
@@ -22,5 +23,12 @@ class Cart(object):
         else:
             self.cart[product_id]['quantity'] += quantity
         self.save()
+
     def save(self):
         self.session.modified = True 
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
