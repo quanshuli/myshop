af339cd3c34356aa04d1f10e57f8f2b4832c5f92
spadeninja <quanshuli.dev@gmail.com>
2021-10-07 21:41:03 +1000

    cart view

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_list_or_404, render, get_object_or_404
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 def product_list(request, category_slug=None):
@@ -23,3 +24,13 @@ def product_detail(request, id, slug):
     return render(request,
                     'shop/product/detail.html',
                     {'product':product})
+
+def product_detail(request, id, slug):
+    product = get_list_or_404(Product, id=id,
+                                       slug=slug,
+                                       available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
