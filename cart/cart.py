476909a7240aa9bb4df52fe447c248c4f9057fe8
spadeninja <quanshuli.dev@gmail.com>
2021-10-07 19:35:31 +1000

    Update cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -4,8 +4,23 @@ from shop.models import Product
 
 class Cart(object):
     def __init__(self, request):
+        # cart is initialized with a request object
+        # current session is stored by:
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
+            # create an empty cart
             cart = self.session[settings.CART_SESSION_ID] = {} 
         self.cart = cart 
+    def add(self, product, quantity=1, override_quantity=False):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity 
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+    def save(self):
+        self.session.modified = True 
