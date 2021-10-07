a9043d5c8f745339fe6b54d52f5b10375e83998d
spadeninja <quanshuli.dev@gmail.com>
2021-10-07 20:31:59 +1000

    Update views.py

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
 
-# Create your views here.
+
