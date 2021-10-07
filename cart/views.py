6a391ab15651f58c1bea6afde339e2f6bed0b937
spadeninja <quanshuli.dev@gmail.com>
2021-10-07 20:40:24 +1000

    Update views.py

--- cart/views.py
+++ cart/views.py
@@ -4,4 +4,16 @@ from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 
+#only allow POST request
+@require_POST
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_list_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
 
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
+
+    
