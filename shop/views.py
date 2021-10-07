611c757af305c17da2532b9b4ca4f3c1d77c2230
spadeninja <quanshuli.dev@gmail.com>
2021-10-08 08:10:49 +1000

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -16,14 +16,14 @@ def product_list(request, category_slug=None):
                     'categories': categories,
                     'products': products})
 
-def product_detail(request, id, slug):
+'''def product_detail(request, id, slug):
     product = get_object_or_404(Product,
                                 id=id,
                                 slug=slug,
                                 available=True)
     return render(request,
                     'shop/product/detail.html',
-                    {'product':product})
+                    {'product':product})'''
 
 def product_detail(request, id, slug):
     product = get_list_or_404(Product, id=id,
