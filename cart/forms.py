0249a24e915d455c7744ca2b0235f5fc9266ae7a
spadeninja <quanshuli.dev@gmail.com>
2021-10-08 15:24:30 +1000

    order

--- cart/forms.py
+++ cart/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Order 
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
@@ -9,3 +10,11 @@ class CartAddProductForm(forms.Form):
     override = forms.BooleanField(required=False,
                                   initial=False,
                                   widget=forms.HiddenInput)
+
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address',
+                  'postal_code', 'city']
+
+                  
