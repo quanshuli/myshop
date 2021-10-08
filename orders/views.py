af838fcd04b8b1954076af5ba338ce91c007cbcd
spadeninja <quanshuli.dev@gmail.com>
2021-10-08 16:07:12 +1000

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -18,8 +18,8 @@ def order_create(request):
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
-        else:
-            form = OrderCreateForm()
+    else:
+        form = OrderCreateForm()
     return render(request,
-                  'orders/order/create.html',
-                  {'cart':cart, 'form':form})
+                'orders/order/create.html',
+                {'cart':cart, 'form':form})
